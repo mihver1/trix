e7a2dd9218114568b7cb9e9b414d9b79f5105c8e
mihver <mihver@macbook-pro-mihver.caracara-cirius.ts.net>
2026-05-12 19:04:32 +0300

    bump version to 0.2.2

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -92,7 +92,7 @@ android {
         minSdk = 28
         targetSdk = 36
         versionCode = 1
-        versionName = "0.2.1"
+        versionName = "0.2.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         testInstrumentationRunnerArguments["trixBaseUrl"] = trixInteropBaseUrl
         buildConfigField("String", "TRIX_BASE_URL", "\"$trixBaseUrl\"")
